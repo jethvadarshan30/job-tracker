f2965bedc3e3761c59a6315be36f2b9b9b657acc
Darshan Jethva <darshanjethva@Darshans-Laptop.local>
2026-04-02 19:32:08 -0700

    Update frontend API URL to Render backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:8000' })
+const api = axios.create({ baseURL: 'https://job-tracker-backend-2sdr.onrender.com' })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
